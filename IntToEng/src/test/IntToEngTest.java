de94859500adcd6293a2efff7017c1e4ac361b3b
g16938sm <[g16938sm@gm.tsuda.ac.jp]>
2018-05-22 14:24:47 +0900

    ドライバー：keey21akashi ナビゲーター：misaki3
    ３回目
    ２桁と100が英訳できる

--- IntToEng/src/test/IntToEngTest.java
+++ IntToEng/src/test/IntToEngTest.java
@@ -17,10 +17,31 @@ public class IntToEngTest {
 		assertThat(actual, is(expected));
 	}
 	@Test
+	public void 整数値30がthirtyと変換される() {
+		IntToEng ite = new IntToEng();
+		String expected = "thirty";
+		String actual = ite.translateEng2(30);
+		assertThat(actual, is(expected));
+	}
+	@Test
 	public void 整数値34がthirtyfourと変換される() {
 		IntToEng ite = new IntToEng();
 		String expected = "thirty four";
-		String actual = ite.translateEng2(34);
+		String actual = ite.translateEng2(34) +  " " + ite.translateEng(34);
+		assertThat(actual, is(expected));
+	}
+	@Test
+	public void 整数値12がtwelveと変換される() {
+		IntToEng ite = new IntToEng();
+		String expected = "twelve";
+		String actual = ite.translateEng3(12);
+		assertThat(actual, is(expected));
+	}
+	@Test
+	public void 整数値100がonehundredと変換される() {
+		IntToEng ite = new IntToEng();
+		String expected = "one hundred";
+		String actual = ite.translateEng4(100);
 		assertThat(actual, is(expected));
 	}
 
